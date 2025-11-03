f2569da9882df9770edc2e1cbce7d19622f2f67e
erika0915 <soohee2001@gmail.com>
2025-11-03 09:37:40 +0900

    fix: InputValidator의 메서드에 static 처리

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -13,14 +13,14 @@ public class InputValidator {
     private static final int LOTTO_NUMBER_COUNT = 6;
 
     // 구입 금액 검증
-    public int validatePurchaseAmount(String input){
+    public static int validatePurchaseAmount(String input){
         int amount = validateNumeric(input, "[ERROR] 구입 금액은 숫자여야 합니다.");
         validatePositive(amount);
         validateMultipleOfLottoPrice(amount);
         return amount;
     }
 
-    private int validateNumeric(String input, String errorMessage){
+    private static int validateNumeric(String input, String errorMessage){
         try {
             return Integer.parseInt(input.trim());
         } catch (NumberFormatException e){
@@ -28,20 +28,20 @@ public class InputValidator {
         }
     }
 
-    private void validatePositive(int amount){
+    private static void validatePositive(int amount){
         if (amount < 0){
             throw new IllegalArgumentException("[ERROR] 구입 금액은 0보다 커야 합니다.");
         }
     }
 
-    private void validateMultipleOfLottoPrice(int amount){
+    private static void validateMultipleOfLottoPrice(int amount){
         if (amount % LOTTO_PRICE != 0){
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
     }
 
     // 당첨 번호 검증
-    public List<Integer> validateWinningNumbers(String input){
+    public static List<Integer> validateWinningNumbers(String input){
         String[] numberStrings = input.split(",");
         validateWinningNumberCount(numberStrings);
 
@@ -52,13 +52,13 @@ public class InputValidator {
         return numbers;
     }
 
-    private void validateWinningNumberCount(String[] numberString){
+    private static void validateWinningNumberCount(String[] numberString){
         if (numberString.length != LOTTO_NUMBER_COUNT){
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
         }
     }
 
-    private List<Integer> parseNumbers(String[] numberStrings){
+    private static List<Integer> parseNumbers(String[] numberStrings){
         try{
             List<Integer> numbers = new ArrayList<>();
             for (String s : numberStrings){
@@ -70,33 +70,33 @@ public class InputValidator {
         }
     }
 
-    private void validateNumbersRange(List<Integer> numbers){
+    private static void validateNumbersRange(List<Integer> numbers){
         for (int number : numbers){
             validateNumberRange(number);
         }
     }
-    private void validateNoDuplicates(List<Integer> numbers){
+    private static void validateNoDuplicates(List<Integer> numbers){
         Set<Integer> uniqueNumbers = new HashSet<>();
         if (uniqueNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호느 중복될 수 없습니다.");
         }
     }
 
-    private void validateNumberRange(int number){
+    private static void validateNumberRange(int number){
         if (number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
     // 보너스 번호 검증
-    public int validateBonusNumber(String input, List<Integer> winningNumbers){
+    public static int validateBonusNumber(String input, List<Integer> winningNumbers){
         int bonusNumber = validateNumeric(input, "[ERROR] 보너스 번호는 숫자여야 합니다.");
         validateNumberRange(bonusNumber);
         validateBonusNotInWinningNumbers(bonusNumber, winningNumbers);
         return bonusNumber;
     }
 
-    private void validateBonusNotInWinningNumbers(int bonusNumber, List<Integer> winningNumbers){
+    private static void validateBonusNotInWinningNumbers(int bonusNumber, List<Integer> winningNumbers){
         if (winningNumbers.contains(bonusNumber)){
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
